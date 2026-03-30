6590ae5dd1e97cebd5d8a75918e4fa5c45385bb0
Franco Barberis <francobarberissic3@gmail.com>
2026-03-30 14:56:16 -0300

    Boilerplate for exercises and routines

---
+++ gym-app/src/main/java/com/gymplanner/gym_app/users/usecases/modification/EditUser.java
@@ -0,0 +1,17 @@
+package com.gymplanner.gym_app.users.usecases.modification;
+
+import com.gymplanner.gym_app.users.domain.User;
+import com.gymplanner.gym_app.users.ports.EmailService;
+import com.gymplanner.gym_app.users.ports.UserRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class EditUser {
+    private final UserRepository UserRepository;
+    private final EmailService EmailService;
+
+    public User execute(){
+        return null;
+    }
+}
